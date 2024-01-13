2affff4c4dfad812683152968faf28efc18c6fda
Sqooby <michal.basznianin3@gmail.com>
2024-01-13 13:51:38 +0100

    added product model

---
+++ lib/models/product_model.dart
@@ -0,0 +1,105 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:equatable/equatable.dart';
+
+class Product extends Equatable {
+  final String id;
+
+  final String name;
+  final String category;
+  final String imageUrl;
+  final double price;
+  final bool isRecommended;
+  final bool isPopular;
+  Product({
+    required this.isPopular,
+    required this.id,
+    required this.name,
+    required this.category,
+    required this.imageUrl,
+    required this.price,
+    required this.isRecommended,
+  });
+  static List<Product> products = [
+    Product(
+      id: '1',
+      name: 'Soft Drink #1',
+      category: 'Soft Drinks',
+      imageUrl:
+          'https://images.unsplash.com/photo-1598614187854-26a60e982dc4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80', //https://unsplash.com/photos/dO9A6mhSZZY
+      price: 2.99,
+      isRecommended: true,
+      isPopular: false,
+    ),
+    Product(
+      id: '2',
+      name: 'Soft Drink #2',
+      category: 'Soft Drinks',
+      imageUrl:
+          'https://images.unsplash.com/photo-1610873167013-2dd675d30ef4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=488&q=80', //https://unsplash.com/photos/Viy_8zHEznk
+      price: 2.99,
+      isRecommended: false,
+      isPopular: true,
+    ),
+    Product(
+      id: '3',
+      name: 'Soft Drink #3',
+      category: 'Soft Drinks',
+      imageUrl:
+          'https://images.unsplash.com/photo-1603833797131-3c0a18fcb6b1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80', //https://unsplash.com/photos/5LIInaqRp5s
+      price: 2.99,
+      isRecommended: true,
+      isPopular: true,
+    ),
+    Product(
+      id: '4',
+      name: 'Smoothies #1',
+      category: 'Smoothies',
+      imageUrl:
+          'https://images.unsplash.com/photo-1526424382096-74a93e105682?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80', //https://unsplash.com/photos/kcYXj4tBtes
+      price: 2.99,
+      isRecommended: true,
+      isPopular: false,
+    ),
+    Product(
+      id: '5',
+      name: 'Smoothies #2',
+      category: 'Smoothies',
+      imageUrl:
+          'https://images.unsplash.com/photo-1505252585461-04db1eb84625?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1552&q=80', //https://unsplash.com/photos/CrK843Pl9a4
+      price: 2.99,
+      isRecommended: false,
+      isPopular: false,
+    ),
+    Product(
+      id: '6',
+      name: 'Smoothies #2',
+      category: 'Smoothies',
+      imageUrl:
+          'https://images.unsplash.com/photo-1505252585461-04db1eb84625?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1552&q=80', //https://unsplash.com/photos/CrK843Pl9a4
+      price: 2.99,
+      isRecommended: false,
+      isPopular: false,
+    ),
+    Product(
+      id: '7',
+      name: 'Smoothies #2',
+      category: 'Smoothies',
+      imageUrl:
+          'https://images.unsplash.com/photo-1505252585461-04db1eb84625?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1552&q=80', //https://unsplash.com/photos/CrK843Pl9a4
+      price: 2.99,
+      isRecommended: false,
+      isPopular: false,
+    ),
+  ];
+
+  @override
+  // TODO: implement props
+  List<Object?> get props => [
+        name,
+        category,
+        imageUrl,
+        price,
+        isRecommended,
+        isPopular,
+      ];
+}
