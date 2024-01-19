72b46832c330f2c5ea3125c0b1d7980af0918520
Sqooby <michal.basznianin3@gmail.com>
2024-01-19 18:47:37 +0100

    init firebase into project

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,16 @@ import 'package:e_commerce/blocs/cart/cart_bloc.dart';
 import 'package:e_commerce/blocs/wishlist/wishlist_bloc.dart';
 import 'package:e_commerce/config/app_router.dart';
 import 'package:e_commerce/config/theme.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'screens/screens.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
